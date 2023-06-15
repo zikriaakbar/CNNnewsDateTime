f5145a4aabdc06917a98108138c673ecfb58058e
zikriaakbar <135124482+zikriaakbar@users.noreply.github.com>
2023-06-15 15:38:28 +0500

    Create PythonCode.py
    Python Code

---
+++ PythonCode.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+# Send a GET request to the CNN website
+url = 'https://www.cnn.com/'
+response = requests.get(url)
+
+# Create a BeautifulSoup object to parse the HTML content
+soup = BeautifulSoup(response.content, 'html.parser')
+
+# Find the news update elements on the page
+news_updates = soup.find_all('span', class_='cd__headline-text')
+
+# Iterate over the news updates and extract the date and time
+for update in news_updates:
+    headline = update.text.strip()
+    timestamp = update.find_previous('span', class_='cd__timestamp').text.strip()
+    print(f"Headline: {headline}")
+    print(f"Timestamp: {timestamp}")
+    print('-' * 50)
